3a97f1511f0c81047a039aaccfb10389c9140ddd
Nilson da Silva <terraskilll@gmail.com>
2016-08-04 17:42:23 -0300

    Player and AI Controller assign

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,21 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName() ));
+	}
+}
 
 
 ATank* ATankPlayerController::GetControlledTank() const
---
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -0,0 +1,22 @@
+
+
+#pragma once
+
+#include "Tank.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+public:
+	virtual void BeginPlay() override;
+	
+	ATank* GetControlledTank() const;
+};
